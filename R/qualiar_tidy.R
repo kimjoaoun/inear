e37a0030df92eece3a4d060ea6e0a357a874fc94
João Pedro <joaopsantos@pm.me>
2020-02-03 21:29:23 -0300

    advancements in qualiar_tidy

--- R/qualiar_tidy.R
+++ R/qualiar_tidy.R
@@ -1,8 +1,31 @@
+#' Title
+#'
+#' @param data_location 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 qualiar_tidy = function(data_location){
   
+  #' .header_removal= function(data){
+  #'   #' This function clean the names of the columns
+  #'   data %<>% janitor::clean_names(case = "lower_camel")
+  #'   data[which(grep("Data da Med", data[1])):nrow(data), ]
+  #' }
   
+  .where_inea = function(data){
+    #' This function finds where is the INEA/Private Company Tag.
+    data[grep("^INEA|^EMPRESA PRIVADA", data$...2):nrow(data), ]
+  }
   
-  xls_files = purrr::map(.x= data_location, .f= openxlsx::read.xlsx)
+  xls_files = purrr::map(.x= data_location, .f= vroom::vroom) %>% 
+    janitor::remove_empty(which = c('cols', 'rows')) %>% 
+    purrr::map(.f = .where_inea)# Do a pre-cleaning
+    
+  
+    
+      
   
   
 }
