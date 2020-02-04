08cbba97b537b562c3c4564b9f9cba4d7be81030
João Pedro <joaopsantos@pm.me>
2020-02-03 21:05:48 -0300

    qualiar_download ok

--- R/qualiar_download.R
+++ R/qualiar_download.R
@@ -10,25 +10,58 @@
 #'
 #' @examples
 qualiar_download = function(years, ret = TRUE){
+  message('Checking if INEA servers are online...')
+  
+  Sys.sleep(0.5) # Let INEA Toaster Server take a break, it might have much requests coming in.
+  
+  if(!RCurl::url.exists("200.20.53.25/qualiar/")){
+    stop("Oops! Looks like INEA servers are offline, or you are not connected to the internet. 
+         Check your connection and try again later.")
+  } else {
+    message("Establishing connection...")
+  }
+  
   
   message(
-    sprintf("Downloading files for years: %s. Please wait, it might take a while..."), 
-    toString(years)
-    )
+    sprintf("Downloading files for year(s): %s. Please wait, it might take a while...", 
+    toString(years)))
 
-  temp = tempfile() %T>% dir.create()
+  temp = tempfile() %T>% 
+    dir.create()
+  
   purrr::iwalk(.x = years,
                .f= ~download.file(
                  url = paste0("200.20.53.25/qualiar/Arquivos/", .x, ".zip"), 
-                 destfile = paste0(temp, "/", .x, ".zip")))
+                 destfile = paste0(temp, "/", .x, ".zip")
+                 )
+               )
   
-  purrr::walk(.x= list.files(temp, pattern = ".zip$"), 
-              .f = function(.x){unzip(x, exdir = paste0(temp, "/", "inear_files"))})
+  purrr::walk(.x= list.files(temp, pattern = ".zip$", full.names = TRUE), 
+              .f = function(x){unzip(x, exdir = paste0(temp, "/inear_files")
+                                     )
+                }
+              )
   
-  message(sprintf("Os arquivos foram salvos em: %s", temp))
+  message("Converting files to CSV, please wait...")
+  
+  suppressMessages(
+    purrr::walk(.x = list.files(paste0(temp, "/inear_files"), full.names = TRUE, pattern = "(.xls|.xlsx)$"), 
+               .f = ~rio::convert(.x, 
+                                 out_file = gsub(x= .x, 
+                                                 pattern=".(xls|xlsx)",
+                                                 replacement = ".csv")
+                                 )
+               )
+    )
+  
+   invisible(file.remove(list.files(paste0(temp, '/inear_files'), 
+                         pattern=".(xls|xlsx)$", 
+                         full.names = TRUE)))
+  
+  message(sprintf("Files saved in: %s", temp)) # Telling the user where the files are stored.
   
   if(ret == TRUE){
-    return(temp)
+    return(paste0(temp, "/inear_files"))
   }
 }
 
